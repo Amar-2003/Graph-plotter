1e01f7e7034a1dafbdbee74ce384bc5a5aba7397
Amarnath K <amarnathk5674@gmail.com>
2021-02-20 16:26:15 +0530

    Created for m

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
-n = float(input("Enter n for the function y = x^n:"))
+n = float(input("Enter n for the function y = mx^n:"))
+m = float(input("Enter m:"))
 i = -100.1
 arrx = []
 arry = []
 while(i < 100):
     arrx.append(i)
-    arry.append(pow(i,n))
-    i += 0.00001
+    arry.append((pow(i,n))*m)
+    i += 1
 plt.plot(arrx,arry)
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
