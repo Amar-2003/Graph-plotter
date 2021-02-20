b73f6715a8a55f2ee227857efe77e0da39b60724
Amarnath K <amarnathk5674@gmail.com>
2021-02-20 13:10:40 +0530

    Created the primitice graph

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import matplotlib.pyplot as plt
 
-#TODO
+n = float(input("Enter n for the function y = x^n:"))
+i = -100.1
+arrx = []
+arry = []
+while(i < 100):
+    arrx.append(i)
+    arry.append(pow(i,n))
+    i += 0.00001
+plt.plot(arrx,arry)
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+
+plt.title("Graph for your function")
+plt.show()
